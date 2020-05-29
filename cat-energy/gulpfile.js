8d73f968cf9a5b9d81295f910795cf3a28b218c8
NekiyWho <nekiydakoma@gmail.com>
2020-05-29 13:27:12 +0600

    gulpfile.js перед настройкой js

--- cat-energy/gulpfile.js
+++ cat-energy/gulpfile.js
@@ -4,7 +4,7 @@ let source_folder = "#src";
 let path = {
     build: {
         html: project_folder + "/",
-        css: project_folder + "css/",
+        css: project_folder + "/css/",
         js: project_folder + "/js/",
         img: project_folder + "/img",
         fonts: project_folder + "/fonts",
@@ -27,8 +27,14 @@ let path = {
 
 let { src, dest } = require('gulp'),
     gulp = require('gulp'),
-    browsersync = require('browser-sync').create();
-    fileinclude = require("gulp-file-include");
+    browsersync = require('browser-sync').create(),
+    fileinclude = require("gulp-file-include"),
+    del = require("del"),
+    scss = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    group_media = require("gulp-group-css-media-queries"),
+    clean_css = require("gulp-clean-css"),
+    rename = require("gulp-rename"); 
 
 function browserSync(params) {
     browsersync.init({
@@ -47,13 +53,46 @@ function html() {
         .pipe(browsersync.stream())
 }
 
-function watchFiles(params) {
-    gulp.watch([path.watch.html], html);
+function css() {
+    return src(path.src.css)
+        .pipe(
+            scss({
+                outputStyle: "expanded"
+            })
+        )
+        .pipe(
+            group_media()
+        )
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ["last 5 versions"],
+                cascade: true
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(clean_css())
+        .pipe(
+            rename({
+                extname: ".min.css"
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream())
 }
 
-let build = gulp.series(html);
+function watchFiles(params) {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+}
+
+function clean(params) {
+    return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(html, css));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
