5fcef52f79cbe9edc5101861b00b8dc627753507
NekiyWho <nekiydakoma@gmail.com>
2020-06-01 14:55:55 +0600

    начал стилизовать cat-energy

--- cat-energy/gulpfile.js
+++ cat-energy/gulpfile.js
@@ -34,7 +34,12 @@ let { src, dest } = require('gulp'),
     autoprefixer = require("gulp-autoprefixer"),
     group_media = require("gulp-group-css-media-queries"),
     clean_css = require("gulp-clean-css"),
-    rename = require("gulp-rename"); 
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    Imagemin = require("gulp-imagemin"),
+    webp = require("gulp-webp");
+    //webphtml = require("gulp-webp-html"),
+    //webpcss = require("gulp-webpcss");
 
 function browserSync(params) {
     browsersync.init({
@@ -49,6 +54,7 @@ function browserSync(params) {
 function html() {
     return src(path.src.html)
         .pipe(fileinclude())
+        //.pipe(webphtml())
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
 }
@@ -69,6 +75,7 @@ function css() {
                 cascade: true
             })
         )
+        //.pipe(webpcss())
         .pipe(dest(path.build.css))
         .pipe(clean_css())
         .pipe(
@@ -80,18 +87,59 @@ function css() {
         .pipe(browsersync.stream())
 }
 
+function js() {
+    return src(path.src.js)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.js))
+        .pipe(
+            uglify()
+        )
+        .pipe(
+            rename({
+                extname: ".min.js"
+            })
+        )
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream())
+}
+
+function images() {
+    return src(path.src.img)
+        .pipe(
+            webp({
+                quality: 70
+            })
+        )
+        .pipe(dest(path.build.img))
+        .pipe(src(path.src.img))
+        .pipe(
+            Imagemin({
+                progressive: true,
+                svgoPlugins: [{removeViewBox: false}],
+                interlaced: true,
+                optimizationLevel: 3
+            })
+        )
+        .pipe(dest(path.build.img))
+        .pipe(browsersync.stream())
+}
+
 function watchFiles(params) {
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.img], images);
 }
 
 function clean(params) {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(html, css));
+let build = gulp.series(clean, gulp.parallel(js, html, css, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.images = images;
+exports.js = js;
 exports.css = css;
 exports.html = html;
 exports.build = build;
